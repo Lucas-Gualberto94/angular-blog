b788f8233fe667202b1ca1561e19f879abc4d4e9
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2024-01-05 15:35:23 +0000

    passing images as absolute path

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,25 +1,25 @@
 export const dataFake = [
   {
     "id": "1",
-    "photo": "../../assets/scienceReligion1.jpg",
+    "photo": "/../../assets/scienceReligion1.jpg",
     "title": "Are science and religion compatible?",
     "category": "science"
   },
   {
     "id": "2",
-    "photo": "../../assets/mapKnowledge1.jpg",
+    "photo": "/../../assets/mapKnowledge1.jpg",
     "title": "A map of all knowledge and human experience",
     "category": "philosophy",
   },
   {
     "id": "3",
-    "photo": "../../assets/khun1.jpg",
+    "photo": "/../../assets/khun1.jpg",
     "title": "Thomas Kuhn Theory",
     "category": "philosophy",
   },
   {
     "id": "4",
-    "photo": "../../assets/popper1.png",
+    "photo": "/../../assets/popper1.png",
     "title": "Karl Popper Theory",
     "category": "philosophy",
   },
